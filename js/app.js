ec6b38eea6eff44d2d7a9bc0bf1f20ba235706d5
Oscar-Urena <ouresal0106@g.educaand.es>
2025-11-24 14:35:24 +0100

    Movimiento base +1 y detección de enemigo enfrente

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import * as reglas from "./reglas.js";
 
 const tablero = (() => {
 
@@ -8,19 +9,23 @@ const tablero = (() => {
     const init = () => {
         document.addEventListener("DOMContentLoaded", () => {
             establecerObjetos();
-            establecerEventos();
             crearTablero();
+            establecerObjetos();
             colocarFigurasBlancas();
             colocarFigurasNegras();
+            establecerObjetos();
+            establecerEventos();
         })
     }
 
     const establecerObjetos = () => {
         tablero = document.querySelector("#tablero");
-
+        celdas = document.querySelectorAll(".celda");
     }
     const establecerEventos = () => {
-
+        celdas.forEach(element => {
+            element.addEventListener("click", movimiento);
+        });
     }
 
     const crearTablero = () => {
@@ -38,30 +43,66 @@ const tablero = (() => {
     }
 
     const colocarFigurasBlancas = () => {
-        celdas = document.querySelectorAll(".celda");
         const primeraFila = [...celdas].slice(0, 8);
         const figuras = ["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook"];
         for (let i = 0; i < primeraFila.length; i++) {
-            primeraFila[i].classList.add(figuras[i]+"B");
+            primeraFila[i].classList.add(figuras[i] + "B");
         }
-        const segundaFila = [...celdas].slice(8,16);
+        const segundaFila = [...celdas].slice(8, 16);
         segundaFila.forEach(element => {
             element.classList.add("pawnB");
         });
     }
 
-    const colocarFigurasNegras =() =>{
-        celdas = document.querySelectorAll(".celda");
-        const primeraFila = [...celdas].slice(celdas.length-8, celdas.length);
+    const colocarFigurasNegras = () => {
+        const primeraFila = [...celdas].slice(celdas.length - 8, celdas.length);
         const figuras = ["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook"];
         for (let i = 0; i < primeraFila.length; i++) {
-            primeraFila[i].classList.add(figuras[i]+"N");
+            primeraFila[i].classList.add(figuras[i] + "N");
         }
-        const segundaFila = [...celdas].slice(celdas.length-16,celdas.length-9);
+        const segundaFila = [...celdas].slice(celdas.length - 16, celdas.length - 8);
         segundaFila.forEach(element => {
             element.classList.add("pawnN");
         });
     }
+
+    const movimiento = (e) => {
+        const target = e.target;
+        console.log(target);
+        const posIni = [...celdas].indexOf(target);
+        const estilos = [...celdas[posIni].classList];
+        if (estilos.length > 3) {
+            const color = estilos[3].slice(-1);
+            const pieza = estilos[3];
+
+            switch (color) {
+                case "N": {
+                    if ([...celdas[reglas.movimiento1arriba(posIni)].classList].length > 3) {
+                        console.log("No puedes, ya hay una pieza");
+                    } else {
+                        celdas[posIni].classList.toggle(pieza);
+                        celdas[reglas.movimiento1arriba(posIni)].classList.toggle(pieza);
+                    }
+                    break;
+                }
+                case "B":{
+                    if ([...celdas[reglas.movimiento1abajo(posIni)].classList].length > 3) {
+                        console.log("No puedes, ya hay una pieza");
+                    } else {
+                        celdas[posIni].classList.toggle(pieza);
+                        celdas[reglas.movimiento1abajo(posIni)].classList.toggle(pieza);
+                    }
+                    break;
+                }
+            }
+
+        }
+        else {
+            console.log("Selecciona uno que tenga ficha")
+        }
+    }
+
+
     return {
         init,
     }
---
+++ js/reglas.js
@@ -0,0 +1,18 @@
+"use strict";
+
+export const movimiento1abajo=(x,n)=>{
+    return x+8;
+}
+
+export const movimiento2abajo=(x,n)=>{
+    return x+16;
+}
+
+export const movimiento1arriba=(x,n)=>{
+    return x-8;
+}
+
+export const movimiento2arriba=(x,n)=>{
+    return x-16;
+}
+
