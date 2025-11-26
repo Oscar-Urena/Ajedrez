38018968056954699ecf65765f35a6b34b6c8b01
Oscar-Urena <ouresal0106@g.educaand.es>
2025-11-26 12:06:39 +0100

    Figuras blancas como sub div creado

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ const tablero = (() => {
             toogle = !toogle;
             for (let j = 0; j < 8; j++) {
                 celda = document.createElement("div");
-                celda.classList.add("celda", "figura");
+                celda.classList.add("celda");
                 celda.classList.add((toogle) ? "blanco" : "negro");
                 toogle = !toogle;
                 tablero.append(celda);
@@ -46,7 +46,9 @@ const tablero = (() => {
         const primeraFila = [...celdas].slice(0, 8);
         const figuras = ["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook"];
         for (let i = 0; i < primeraFila.length; i++) {
-            primeraFila[i].classList.add(figuras[i] + "B");
+            const figura = document.createElement("div");
+            figura.classList.add(figuras[i] + "B", "figura");
+            primeraFila[i].append(figura);
         }
         const segundaFila = [...celdas].slice(8, 16);
         segundaFila.forEach(element => {
